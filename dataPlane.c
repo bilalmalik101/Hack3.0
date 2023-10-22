6290111e8f19a71faf7a23c75d1704d13a73ac25
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-22 21:01:49 +0500

    getting input from user

--- dataPlane.c
+++ dataPlane.c
@@ -3,4 +3,11 @@ int main(){
     // declaring variables
     float monthlydata,useddata,averagedatauseddaily,dailydataused,remainingdatauseddaily,dataexceeding,remainingdata;
     int days,remainingdays;
+    printf("Enter your monthly data package:");
+    scanf("%f",&monthlydata);
+    printf("How many days you used data:");
+    scanf("%i",&days);
+    printf("How much Gb of your data you used:");
+    scanf("%f",&useddata);
+
 }
