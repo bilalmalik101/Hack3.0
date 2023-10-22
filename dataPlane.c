b93d7ee82bbb48062b77b11f425f2b8638813493
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-22 21:16:45 +0500

    show output

--- dataPlane.c
+++ dataPlane.c
@@ -14,8 +14,15 @@ int main(){
     remainingdata=monthlydata-useddata;
     dailydataused=useddata/days;
    if(remainingdata!=0){
+    //declare variables to find how much you over used from your daily data
     remainingdata=remainingdata/remainingdays;
      float exceedDailyUsed=dailydataused-averagedatauseddaily;
      dataexceeding=30*dailydataused;
-
+);
+     printf("To stay below your data plan, use no more than %f GB/day\n", remainingPackage); printf("________________________________________\n");
+     printf("%i days used, %i days remaining\n",days,remainingDays);
+     printf("Average daily use: %f GB/day\n",dailydataused);
+     printf("You are EXCEEDING your average daily use ( %f GB/day)\n",exceedDailyUsed);
+     printf("Continuing this high usage, you'll exceed your data plan by %f GB\n",dataexceeding
+     
 }
