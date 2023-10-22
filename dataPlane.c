0a4635bf148bc048ee0b157eeb13c5bea25bde82
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-22 21:05:29 +0500

    using different variables to get results

--- dataPlane.c
+++ dataPlane.c
@@ -3,11 +3,16 @@ int main(){
     // declaring variables
     float monthlydata,useddata,averagedatauseddaily,dailydataused,remainingdatauseddaily,dataexceeding,remainingdata;
     int days,remainingdays;
+    //getting input from user
     printf("Enter your monthly data package:");
     scanf("%f",&monthlydata);
     printf("How many days you used data:");
     scanf("%i",&days);
     printf("How much Gb of your data you used:");
     scanf("%f",&useddata);
+    remainingDays=30-days;
+    remainingdata=monthlydata-useddata;
+    dailydataused=useddata/days;
+    
 
 }
