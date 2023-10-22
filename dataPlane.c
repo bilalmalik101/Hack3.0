b605ac309d1139e7d69d81dd07132bfd3bf6c266
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-22 20:58:48 +0500

    dataPlane.c declaring variables

---
+++ dataPlane.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    // declaring variables
+    float monthlydata,useddata,averagedatauseddaily,dailydataused,remainingdatauseddaily,dataexceeding,remainingdata;
+    int days,remainingdays;
+}
